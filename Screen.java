6c37388666a04168546b2a8581204f62989bb915
Amanda Zhang <amandazhanglele@gmail.com>
2023-02-08 14:17:16 -0800

    add gene pool

--- Screen.java
+++ Screen.java
@@ -1,8 +1,11 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.*;
 import java.awt.event.*;
 import java.util.*;
 import java.io.*;
+import javax.imageio.ImageIO;
+import java.net.*;
 
 public class Screen extends JPanel implements ActionListener {
 
@@ -10,6 +13,7 @@ public class Screen extends JPanel implements ActionListener {
     private double P, Q, AA, Aa, aa;
     private int numOfAlleles;
     private JButton updateButton;
+    private BufferedImage pool, whiteCircle, redCircle;
 
     public Screen() throws IOException {
         setLayout(null);
@@ -35,6 +39,10 @@ public class Screen extends JPanel implements ActionListener {
         Q = 0.3;
         numOfAlleles = 200;
 
+        pool = ImageIO.read(new File("pool.jpeg"));
+        whiteCircle = ImageIO.read(new File("whitecircle.png"));
+        redCircle = ImageIO.read(new File("redcircle.png"));
+
         repaint();
     }
 
@@ -42,6 +50,11 @@ public class Screen extends JPanel implements ActionListener {
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
+        g.setColor(Color.white);
+        g.fillRect(0, 0, 1000, 1000);
+        g.drawImage(pool, 100, 300, null);
+
+        g.setColor(Color.blue);
         g.drawString("P:", 50, 40);
         g.drawString("Q:", 150, 40);
 
@@ -50,9 +63,16 @@ public class Screen extends JPanel implements ActionListener {
         aa = Q * Q;
 
         g.drawString("Total number of alleles: " + numOfAlleles, 50, 100);
+        g.setColor(Color.red);
         g.drawString("AA: " + AA, 50, 130);
         g.drawString("Aa: " + Aa, 50, 160);
+        g.setColor(Color.black);
         g.drawString("aa: " + aa, 50, 190);
+
+        g.drawImage(whiteCircle, 300, 400, null);
+        g.drawImage(redCircle, 400, 400, null);
+        g.drawString("A: " + Math.round(P*numOfAlleles), 350, 460);
+        g.drawString("a: " + Math.round(Q*numOfAlleles), 450, 460);
 
         repaint();
     }
