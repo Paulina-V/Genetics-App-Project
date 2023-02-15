995d83b71ed180c18d95443370172a7d41d6952a
Amanda Zhang <amandazhanglele@gmail.com>
2023-02-14 16:45:27 -0800

    draw alleles

--- Screen.java
+++ Screen.java
@@ -13,7 +13,9 @@ public class Screen extends JPanel implements ActionListener {
     private double P, Q, AA, Aa, aa;
     private int numOfAlleles;
     private JButton updateButton;
-    private BufferedImage pool, whiteCircle, redCircle;
+    private BufferedImage pool;
+    private ArrayList<String> momList, dadList, populationList;
+    private boolean redrawAlleles = true;
 
     public Screen() throws IOException {
         setLayout(null);
@@ -39,9 +41,31 @@ public class Screen extends JPanel implements ActionListener {
         Q = 0.3;
         numOfAlleles = 200;
 
+        int countA = (int)((numOfAlleles/2) * P);
+        int counta = (int)((numOfAlleles/2) * Q);
+        momList = new ArrayList<>();
+        dadList = new ArrayList<>();
+        for (int i=0; i<countA; i++){
+            momList.add("A");
+            dadList.add("A");
+        }
+        for (int i=0; i<counta; i++){
+            momList.add("a");
+            dadList.add("a");
+        }
+        Collections.shuffle(momList);
+        Collections.shuffle(dadList);
+
+        populationList = new ArrayList<>();
+        for (int i=0; i<momList.size(); i++){
+            String allele = momList.get(i) + dadList.get(i);
+            if (allele.equals("aA")){
+                allele = "Aa";
+            }
+            populationList.add(allele);
+        }
+
         pool = ImageIO.read(new File("pool.jpeg"));
-        whiteCircle = ImageIO.read(new File("whitecircle.png"));
-        redCircle = ImageIO.read(new File("redcircle.png"));
 
         repaint();
     }
@@ -52,15 +76,13 @@ public class Screen extends JPanel implements ActionListener {
 
         g.setColor(Color.white);
         g.fillRect(0, 0, 1000, 1000);
-        g.drawImage(pool, 100, 300, null);
+        g.drawImage(pool, 450, 50, null);
 
         g.setColor(Color.blue);
         g.drawString("P:", 50, 40);
         g.drawString("Q:", 150, 40);
 
-        AA = P * P;
-        Aa = 2 * P * Q;
-        aa = Q * Q;
+        updateAlleles();
 
         g.drawString("Total number of alleles: " + numOfAlleles, 50, 100);
         g.setColor(Color.red);
@@ -69,18 +91,75 @@ public class Screen extends JPanel implements ActionListener {
         g.setColor(Color.black);
         g.drawString("aa: " + aa, 50, 190);
 
-        g.drawImage(whiteCircle, 300, 400, null);
-        g.drawImage(redCircle, 400, 400, null);
-        g.drawString("A: " + Math.round(P*numOfAlleles), 350, 460);
-        g.drawString("a: " + Math.round(Q*numOfAlleles), 450, 460);
+        g.setColor(Color.gray);
+        g.fillOval(550, 100, 50, 50);
+        g.setColor(Color.red);
+        g.fillOval(630, 100, 50, 50);
+        g.setColor(Color.black);
+        g.drawString("A: " + Math.round(P*numOfAlleles), 560, 130);
+        g.drawString("a: " + Math.round(Q*numOfAlleles), 640, 130);
 
-        repaint();
+        if (redrawAlleles){
+            drawAlleles(g);
+            System.out.println(1);
+        }
     }
 
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == updateButton){
             P = Double.valueOf(pInput.getText());
             Q = Double.valueOf(qInput.getText());
+
+            redrawAlleles = true;
+            updateAlleles();
+            repaint();
+        }
+    }
+
+    public void drawAlleles(Graphics g){
+        for (int i=0; i<populationList.size(); i++){
+            int x = (int)((Math.random()*650) + 50);
+            int y = (int)((Math.random()*400) + 250);
+            if (populationList.get(i).equals("aa")){
+                g.setColor(Color.gray);
+            } else {
+                g.setColor(Color.red);
+            }
+            g.fillOval(x, y, 50, 50);
+            g.setColor(Color.black);
+            g.drawString(populationList.get(i), x+20, y+20);
+            System.out.println(2);
+        }
+        redrawAlleles = false;
+    }
+
+    public void updateAlleles(){
+        AA = P * P;
+        Aa = 2 * P * Q;
+        aa = Q * Q;
+
+        int countA = (int)((numOfAlleles/2) * P);
+        int counta = (int)((numOfAlleles/2) * Q);
+        momList = new ArrayList<>();
+        dadList = new ArrayList<>();
+        for (int i=0; i<countA; i++){
+            momList.add("A");
+            dadList.add("A");
+        }
+        for (int i=0; i<counta; i++){
+            momList.add("a");
+            dadList.add("a");
+        }
+        Collections.shuffle(momList);
+        Collections.shuffle(dadList);
+
+        populationList = new ArrayList<>();
+        for (int i=0; i<momList.size(); i++){
+            String allele = momList.get(i) + dadList.get(i);
+            if (allele.equals("aA")){
+                allele = "Aa";
+            }
+            populationList.add(allele);
         }
     }
     
