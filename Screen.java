c667f92a2561de9212574df2b0c0555a6d7de00b
Amanda Zhang <amandazhanglele@gmail.com>
2023-03-10 12:43:16 -0800

    re draw

--- Screen.java
+++ Screen.java
@@ -9,10 +9,10 @@ import java.net.*;
 
 public class Screen extends JPanel implements ActionListener {
 
-    private JTextField pInput, qInput;
+    private JTextField pInput, qInput, killAAInput,killAaInput, killaaInput; 
     private double P, Q, AA, Aa, aa;
     private int numOfAlleles;
-    private JButton updateButton;
+    private JButton updateButton, killButton, reproduceButton;
     private BufferedImage pool;
     private ArrayList<String> momList, dadList, populationList;
     private boolean redrawAlleles = true;
@@ -37,9 +37,36 @@ public class Screen extends JPanel implements ActionListener {
         this.add(updateButton);
         updateButton.addActionListener(this);
 
+        reproduceButton = new JButton();
+        reproduceButton.setBounds(250, 100, 200, 30);
+        reproduceButton.setText("REPRODUCE POPULATION");
+        this.add(reproduceButton);
+        reproduceButton.addActionListener(this);
+
+        killAAInput = new JTextField();
+        killAAInput.setBounds(50, 800, 100, 30);
+        killAAInput.setText("AA%");
+        this.add(killAAInput);
+
+        killAaInput = new JTextField();
+        killAaInput.setBounds(150, 800, 100, 30);
+        killAaInput.setText("Aa%");
+        this.add(killAaInput);
+
+        killaaInput = new JTextField();
+        killaaInput.setBounds(250, 800, 100, 30);
+        killaaInput.setText("aa%");
+        this.add(killaaInput);
+
+        killButton = new JButton();
+        killButton.setBounds(350, 800, 280, 30);
+        killButton.setText("Perform Natral Selection");
+        this.add(killButton);
+        killButton.addActionListener(this);
+
         P = 0.7;
         Q = 0.3;
-        numOfAlleles = 200;
+        numOfAlleles = 20000;
 
         int countA = (int)((numOfAlleles/2) * P);
         int counta = (int)((numOfAlleles/2) * Q);
@@ -82,9 +109,10 @@ public class Screen extends JPanel implements ActionListener {
         g.drawString("P:", 50, 40);
         g.drawString("Q:", 150, 40);
 
+
         updateAlleles();
 
-        g.drawString("Total number of alleles: " + numOfAlleles, 50, 100);
+        g.drawString("Total population: " + numOfAlleles/2, 50, 100);
         g.setColor(Color.red);
         g.drawString("AA: " + AA, 50, 130);
         g.drawString("Aa: " + Aa, 50, 160);
@@ -99,10 +127,7 @@ public class Screen extends JPanel implements ActionListener {
         g.drawString("A: " + Math.round(P*numOfAlleles), 560, 130);
         g.drawString("a: " + Math.round(Q*numOfAlleles), 640, 130);
 
-        if (redrawAlleles){
-            drawAlleles(g);
-            System.out.println(1);
-        }
+        drawAlleles(g);
     }
 
     public void actionPerformed(ActionEvent e) {
@@ -114,29 +139,98 @@ public class Screen extends JPanel implements ActionListener {
             updateAlleles();
             repaint();
         }
+        if(e.getSource() == killButton){
+            naturalSelection(Integer.parseInt(killAAInput.getText()), Integer.parseInt(killAaInput.getText()), Integer.parseInt(killaaInput.getText()));
+            updateAlleles();
+            repaint();
+        }
+        if(e.getSource() == reproduceButton){
+            numOfAlleles = 2*10000;
+
+        }
     }
 
     public void drawAlleles(Graphics g){
+        // for (int i=0; i<populationList.size(); i++){
+        //     int x = (int)((Math.random()*650) + 50);
+        //     int y = (int)((Math.random()*400) + 250);
+        //     if (populationList.get(i).equals("aa")){
+        //         g.setColor(Color.gray);
+        //     } else {
+        //         g.setColor(Color.red);
+        //     }
+        //     g.fillOval(x, y, 50, 50);
+        //     g.setColor(Color.black);
+        //     g.drawString(populationList.get(i), x+20, y+20);
+        //     System.out.println(2);
+        // }
+        // redrawAlleles = false;
+
+        int AA_ct = 0;
+        int Aa_ct = 0;
+        int aa_ct = 0;
+
         for (int i=0; i<populationList.size(); i++){
-            int x = (int)((Math.random()*650) + 50);
-            int y = (int)((Math.random()*400) + 250);
             if (populationList.get(i).equals("aa")){
-                g.setColor(Color.gray);
+                aa_ct++;
+            } else if (populationList.get(i).equals("Aa")){
+                Aa_ct++;
             } else {
-                g.setColor(Color.red);
+                AA_ct++;
             }
-            g.fillOval(x, y, 50, 50);
-            g.setColor(Color.black);
-            g.drawString(populationList.get(i), x+20, y+20);
-            System.out.println(2);
         }
+        
+        int x = 0;
+        int y = 250;
+        int j = 0;
+
+        for (int i=0; i<AA_ct; i++){
+            g.setColor(Color.red);
+            g.fillOval(x, y, 5, 5);
+            // g.setColor(Color.black);
+            // g.drawString("AA", x+20, y+20);
+            j++;
+            x += 5;
+            if (j%200 == 0) {
+                x = 0;
+                y += 5;
+            }
+        }
+
+        
+        for (int i=0; i<Aa_ct; i++){
+            g.setColor(Color.pink);
+            g.fillOval(x, y, 5, 5);
+            // g.setColor(Color.black);
+            // g.drawString("Aa", x+20, y+20);
+            j++;
+            x += 5;
+            if (j%200 == 0) {
+                x = 0;
+                y += 5;
+            }
+        }
+
+        for (int i=0; i<aa_ct; i++){
+            g.setColor(Color.gray);
+            g.fillOval(x, y, 5, 5);
+            // g.setColor(Color.black);
+            // g.drawString("aa", x+20, y+20);
+            j++;
+            x += 5;
+            if (j%200 == 0) {
+                x = 0;
+                y += 5;
+            }
+        }
+
         redrawAlleles = false;
     }
 
     public void updateAlleles(){
-        AA = P * P;
-        Aa = 2 * P * Q;
-        aa = Q * Q;
+        AA = Math.round(P * P * 100.0)/100.0;
+        Aa = Math.round(2 * P * Q * 100.0)/100.0;
+        aa = Math.round(Q * Q * 100.0)/100.0;
 
         int countA = (int)((numOfAlleles/2) * P);
         int counta = (int)((numOfAlleles/2) * Q);
@@ -161,6 +255,42 @@ public class Screen extends JPanel implements ActionListener {
             }
             populationList.add(allele);
         }
+
+        
     }
-    
+    public void naturalSelection(int AA_in, int Aa_in, int aa_in){
+        System.out.println(AA + ", " + Aa + ", " + aa);
+        double AAdouble = AA_in/100.0;
+        double Aadouble = Aa_in/100.0;
+        double aadouble = aa_in/100.0;
+
+        //System.out.println(AAdouble);
+
+        double AAnum = AA*numOfAlleles*(1-AAdouble);
+        double Aanum = Aa*numOfAlleles*(1-Aadouble);
+        double aanum = aa*numOfAlleles*(1-aadouble);
+        
+        System.out.println("AAnum = " + (AA*numOfAlleles));
+
+        AA = AAnum/numOfAlleles;
+        Aa = Aanum/numOfAlleles;
+        aa = aanum/numOfAlleles;
+
+
+        System.out.println(AA + ", " + Aa+ ", " + aa);
+        
+        P = Math.sqrt(AA);
+        Q = Math.sqrt(aa);
+        double pround = Math.round(P*100.0)/100.0;
+        double qround = Math.round(Q*100.0)/100.0;
+
+        pInput.setText(pround + "");
+        qInput.setText(qround + "");
+
+        System.out.println("in natural selection -- P: " + P + " Q: " + Q);
+        //drawAlleles(g);
+
+        numOfAlleles = AAnum + Aanum + aanum;
+    }
 }
+    
